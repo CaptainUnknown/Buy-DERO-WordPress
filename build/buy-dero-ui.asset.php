0cb070b2a8bf8acdeaaf91d521c40b3f53998f71
Captain Unknown <captainunknown7@gmail.com>
2022-09-21 22:18:18 +0500

    📦 Build v1.0

---
+++ build/buy-dero-ui.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '8f2632e084a2ad32cfe5');
---
+++ build/buy-dero.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-element'), 'version' => '4b0657de1a3ce16106d6');
